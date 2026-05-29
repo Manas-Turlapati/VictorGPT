3df87ac30b83bac332768c8e2ea580fda27fad36
Manas Turlapati <manasturlapati@gmail.com>
2026-05-29 14:04:04 +0530

    fix auth header case sensitivity

--- Backend/middleware.js
+++ Backend/middleware.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const verifyToken = (req,res,next)=>{
-    const token = req.headers.authorization?.split(' ')[1];
+    const authHeader = req.headers.authorization || req.headers.Authorization
+    const token = authHeader?.split(' ')[1];
     if(!token){
         return res.status(400).json({ msg: "Tokens are not generated!Access denied" });
     }
