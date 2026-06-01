f3cc1cfdab4e885afcbd639ac9d707134b34e339
Manas Turlapati <manasturlapati@gmail.com>
2026-06-01 12:10:23 +0530

    new commit

--- Backend/server.js
+++ Backend/server.js
@@ -11,7 +11,7 @@ const PORT = 8080;
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://victor-gpt-smoky.vercel.app"],
   }),
 );
 //connecting mongodb with the terminal 
